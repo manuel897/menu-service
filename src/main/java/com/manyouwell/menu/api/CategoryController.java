4096aab9ce3fc220aba77ce05dd2a355c7ad5e93
Manuel Mangalathu <manuelmangalathu@gmail.com>
2022-09-01 10:54:05 +0200

    update category controller

--- src/main/java/com/manyouwell/menu/api/CategoryController.java
+++ src/main/java/com/manyouwell/menu/api/CategoryController.java
@@ -1,16 +1,20 @@
 package com.manyouwell.menu.api;
 
 import com.manyouwell.menu.model.Category;
+import com.manyouwell.menu.payload.response.MessageResponse;
 import com.manyouwell.menu.service.MenuService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
+@CrossOrigin("https://lordstdpa.com")
 @RequestMapping("category")
 public class CategoryController {
+    private static Logger logger = LogManager.getLogger(CategoryController.class);
+
     private final MenuService menuService;
 
     @Autowired
@@ -18,23 +22,24 @@ public class CategoryController {
         this.menuService = menuService;
     }
 
-    @CrossOrigin(origins = "*")
     @GetMapping
-    public List<Category> findAll() {
-        return this.menuService.findAll();
+    public ResponseEntity<?> findAll() {
+        return ResponseEntity.ok(this.menuService.findAll());
     }
 
-    @CrossOrigin(origins = "*")
     @PostMapping("/reset")
     public ResponseEntity<Category[]> resetMenu(@RequestBody Category[] categoryReq) {
         // TODO validate request body
         return ResponseEntity.ok(this.menuService.resetMenuCard(categoryReq));
     }
 
-    @CrossOrigin(origins = "*")
     @PostMapping("/add")
-    public ResponseEntity<Category> insertCategory(@RequestBody Category categoryReq) {
+    public ResponseEntity<?> insertCategory(@RequestBody Category categoryReq) {
         // TODO validate request body
+        if(this.menuService.existsByCategoryName(categoryReq.getName())) {
+            return ResponseEntity.badRequest()
+                    .body(new MessageResponse(String.format("Category %s already exists", categoryReq.getName())));
+        }
         return ResponseEntity.ok(this.menuService.insertCategory(categoryReq));
     }
 }
--- src/main/java/com/manyouwell/menu/dao/CategoryRepo.java
+++ src/main/java/com/manyouwell/menu/dao/CategoryRepo.java
@@ -4,5 +4,5 @@ import com.manyouwell.menu.model.Category;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CategoryRepo extends MongoRepository<Category, String> {
-
+    Boolean existsByName(String name);
 }
